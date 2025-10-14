d32e07b6e03f8121c4b75b0567200b3b8d7af1df
Dima Shalupnya <dshalupnya@gmail.com>
2025-10-14 15:27:59 +0200

    Mostly functioning silabear function

--- pi_lenguage.py
+++ pi_lenguage.py
@@ -1,11 +1,60 @@
+consonantes = [
+    "b", "c", "d", "f", "g", "h", "j", "k", "l", "ll",
+    "m", "n", "ñ", "p", "q", "r", "s", "t", "v", "w", "x", "z"
+]
+
+vocales_abiertas = ["a", "e", "o"]
+
+vocales_cerradas = ["i", "u"]
+
+consonantesDobles = ["bl", "cl", "fl", "gl", "kl", "pl", "tl",
+                "br", "cr", "dr", "fr", "gr", "kr", "pr", "tr",
+                "ch", "ll", "rr"]
+
 def silabear(palabra:str) -> list:
-    pass
+    word = list(palabra)
+    silabas = []
+    posVoc = {}
+    contador = 0
+    while any(x is not None for x in word):
+        for i in word:
+            if i  in vocales_abiertas or i in vocales_cerradas:
+                pos= word.index(i)
+                
+                
+                if word[pos-1]:
+                    nl = word[pos -1]
+                    silabas.append(nl + i)
+                    posVoc[pos] = nl + i
+                    word[pos] = None
+                    word[pos-1] = None 
+                    
+                else:
+                   silabas.append(i)
+            if contador >= 1 and i in consonantes:
+                consPos= word.index(i)
+                posVoc[consPos-1] = posVoc[consPos-1] + i
+                word[consPos] = None
+
+        contador += 1    
+
+    silabas = [x for i,x in posVoc.items()]
+
+        
+                
+    return silabas               
+
+                
+print(silabear("cotarte"))                   
+
+   
+    
 
 
 def normal_a_pi(palabra: str) -> str:
+    pass
 
     
-    pass
 
 def pi_a_normal(palabra: str) -> str:
     pass
